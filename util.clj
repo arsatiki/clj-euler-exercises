f719478527f8b8068b1d5813884509413c14104b
Antti Rasinen <ars@iki.fi>
2009-10-10 13:03:38 +0300

    destructuring bind makes primes shorter

--- util.clj
+++ util.clj
@@ -2,9 +2,8 @@
 ;;   where
 ;;   sieve (p:x) = p : sieve [ n | n <- x, n `mod` p > 0 ]  
 ;; Inspired by Haskell.
-(defn- sieve [coll]
-  (let [p (first coll)
-        x (filter #(> (mod % p) 0) (rest coll))]
+(defn- sieve [[p & coll]]
+  (let [x (filter #(> (mod % p) 0) coll)]
     (cons p (lazy-seq (sieve x)))))
 
 (def primes (sieve (iterate inc 2)))
